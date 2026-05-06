9b7cac605cd9b143408161606c56cb02424eeec4
Kitkatisvibing <kate.jadi@gmail.com>
2026-05-06 10:14:28 +0200

    a

--- server.js
+++ server.js
@@ -1,5 +1,101 @@
-console.log('Hier komt je server voor Sprint 10.')
+// Importeer het npm package Express (uit de door npm aangemaakte node_modules map)
+// Deze package is geïnstalleerd via `npm install`, en staat als 'dependency' in package.json
+import express from 'express'
 
-console.log('Gebruik uit Sprint 9 alleen de code die je mee wilt nemen.')
+// Importeer de Liquid package (ook als dependency via npm geïnstalleerd)
+import { Liquid } from 'liquidjs';
 
-console.log('Zet \'m op!')
+// Maak een nieuwe Express applicatie aan, waarin we de server configureren
+const app = express()
+
+// Maak werken met data uit formulieren iets prettiger
+app.use(express.urlencoded({extended: true}))
+
+// Gebruik de map 'public' voor statische bestanden (resources zoals CSS, JavaScript, afbeeldingen en fonts)
+// Bestanden in deze map kunnen dus door de browser gebruikt worden
+app.use(express.static('public'))
+
+// Stel Liquid in als 'view engine'
+const engine = new Liquid()
+app.engine('liquid', engine.express())
+
+// Stel de map met Liquid templates in
+// Let op: de browser kan deze bestanden niet rechtstreeks laden (zoals voorheen met HTML bestanden)
+app.set('views', './views')
+
+
+app.get('/', async function (request, response) {
+
+  response.render('index.liquid')
+})
+
+app.get('/instrumenten', async function (request, response) {
+
+  const type = request.query.instrument
+
+  const instrumentsResponse = await fetch('https://fdnd-agency.directus.app/items/preludefonds_instruments/?')
+
+  const instrumentsResponseJSON = await instrumentsResponse.json()
+  
+  response.render('instrumenten.liquid', {
+    instrumenten: instrumentsResponseJSON.data,
+  })
+})
+
+app.get('/instrumenten/:key', async function (request, response) {
+
+  const instrumentResponse = await fetch('https://fdnd-agency.directus.app/items/preludefonds_instruments/?filter[key]=' + request.params.key)
+  const instrumentResponseJSON = await instrumentResponse.json()
+
+  response.render('instrumentendetail.liquid', {
+    instrument: instrumentResponseJSON.data[0]
+  })
+})
+
+
+
+
+/*
+// Zie https://expressjs.com/en/5x/api.html#app.post.method over app.post()
+app.post(…, async function (request, response) {
+
+  // In request.body zitten alle formuliervelden die een `name` attribuut hebben in je HTML
+  console.log(request.body)
+
+  // Via een fetch() naar Directus vullen we nieuwe gegevens in
+
+  // Zie https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch over fetch()
+  // Zie https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/JSON/stringify over JSON.stringify()
+  // Zie https://docs.directus.io/reference/items.html#create-an-item over het toevoegen van gegevens in Directus
+  // Zie https://docs.directus.io/reference/items.html#update-an-item over het veranderen van gegevens in Directus
+  const fetchResponse = await fetch(…, {
+    method: …,
+    body: JSON.stringify(…),
+    headers: {
+      'Content-Type': 'application/json;charset=UTF-8'
+    }
+  })
+
+  // Als de POST niet gelukt is, kun je de response loggen. Sowieso een goede debugging strategie.
+  // console.log(fetchResponse)
+
+  // Eventueel kun je de JSON van die response nog debuggen
+  // const fetchResponseJSON = await fetchResponse.json()
+  // console.log(fetchResponseJSON)
+
+  // Redirect de gebruiker daarna naar een logische volgende stap
+  // Zie https://expressjs.com/en/5x/api.html#res.redirect over response.redirect()
+  response.redirect(303, …)
+})
+*/
+
+
+// Stel het poortnummer in waar Express op moet gaan luisteren
+// Lokaal is dit poort 8000; als deze applicatie ergens gehost wordt, waarschijnlijk poort 80
+app.set('port', process.env.PORT || 8000)
+
+// Start Express op, gebruik daarbij het zojuist ingestelde poortnummer op
+app.listen(app.get('port'), function () {
+  // Toon een bericht in de console
+  console.log(`Daarna kun je via http://localhost:${app.get('port')}/ jouw interactieve website bekijken.\n\nThe Web is for Everyone. Maak mooie dingen 🙂`)
+})
